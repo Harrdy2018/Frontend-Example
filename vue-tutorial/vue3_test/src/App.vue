7434f72d3ef3d73d5a7c0c8fe449db24255b431a
lukang-local <lukang-local@example.com>
2022-02-12 15:20:15 +0800

    add learn components

--- vue-tutorial/vue3_test/src/App.vue
+++ vue-tutorial/vue3_test/src/App.vue
@@ -1,45 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
-
-  <h2>name:{{name}}</h2>
-  <h2>age:{{age}}</h2>
-  <h2>sex:{{sex}}</h2>
-  <button @click="sayHello">speak</button>
-  <button @click="sayWelcome">welcome</button>
+  <ExampleOne></ExampleOne>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import {h} from 'vue'
+import ExampleOne from "./components/ExampleOne.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      sex: 'nan'
-    }
-  },
-  methods: {
-    sayWelcome(){
-      alert("welcome to china!");
-    }
-  },
-  setup: function () {
-    let name = "zhangsan";
-    let age = 18;
-
-    function sayHello(){
-      alert(`i am ${name} i ${age}`);
-    }
-
-    return {name,age,sayHello};
-
-    // 返回一个渲染函数，页面以渲染函数为主
-    // return ()=>h('h1', "Hello my render");
+    HelloWorld,
+    ExampleOne
   }
 }
 </script>
---
+++ vue-tutorial/vue3_test/src/components/ExampleOne.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h1>learn setup api</h1>
+        <h2>name:{{name}}</h2>
+        <h2>age:{{age}}</h2>
+        <h2>sex:{{sex}}</h2>
+        <button @click="sayHello">speak</button>
+        <button @click="sayWelcome">welcome</button>
+    </div>
+  </template>
+  
+<script>
+// import {h} from 'vue'
+export default {
+    name: 'ExampleOne',
+    data() {
+        return {
+        sex: 'nan'
+        }
+    },
+    methods: {
+        sayWelcome(){
+        alert("welcome to china!");
+        }
+    },
+    setup: function () {
+        let name = "zhangsan";
+        let age = 18;
+
+        function sayHello(){
+        alert(`i am ${name} i ${age}`);
+        }
+
+        return {name,age,sayHello};
+
+        // 返回一个渲染函数，页面以渲染函数为主
+        // return ()=>h('h1', "Hello my render");
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
