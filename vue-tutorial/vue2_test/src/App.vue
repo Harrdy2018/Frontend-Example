50068ffb5f575e887e4bbd45e3d717bdecd6f9c4
lukang-local <lukang-local@example.com>
2022-02-13 23:32:26 +0800

    add slots

--- vue-tutorial/vue2_test/src/App.vue
+++ vue-tutorial/vue2_test/src/App.vue
@@ -4,18 +4,32 @@
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <p>-------------------------------------------------------------------</p>
     <ExampleOne/>
+    <p>-------------------------------------------------------------------</p>
+    <ExampleTwo msg="hello mi" school="NJUPT">
+      <!--子组件 $slots 接收插槽-->
+      <p>默认插槽AAA</p>
+      <p>默认插槽BBB</p>
+      <template slot="testA">
+        <p>具名插槽AAA</p>
+      </template>
+      <template slot="testB">
+        <p>具名插槽BBB</p>
+      </template>
+    </ExampleTwo>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import ExampleOne from './components/ExampleOne.vue'
+import ExampleTwo from './components/ExampleTwo.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    ExampleOne
+    ExampleOne,
+    ExampleTwo,
   }
 }
 </script>
---
+++ vue-tutorial/vue2_test/src/components/ExampleTwo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="hello">
+      <h1>父组件给子组件传值</h1>
+      <h2>msg:{{ msg }}</h2>
+      <h2>school:{{ school }}</h2>
+      <slot></slot>
+      <slot name="testA"></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ExampleTwo',
+    props: {
+      msg: String,
+      school: String
+    },
+    mounted(){
+        console.log("mounted ",this);
+    }
+  }
+  </script>
+  <style scoped>
+  </style>
+  
